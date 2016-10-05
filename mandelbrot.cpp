533da838e58e6892ea58c2492e3cdd5920901f8b
Morast <anthonymorast33@hotmail.com>
2016-10-05 10:34:57 -0600

    Initialize openGl and glut. Open window and track mouse movement

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -2,21 +2,40 @@
 
 int main( int argc, char* argv[] )
 {
-	
+	// Initialize glut/openGL
+	glutInit( &argc, argv );
+	initOpenGL();
+
+	// enter glut main loop
+	glutMainLoop();
+
+	/*
+		"To keep the compiler from bitching."
+			- Dr. John Weiss
+	*/
 	return 0;
 }
 
 /*******************************************************************************
  *								OpenGL Functions							   *
  ******************************************************************************/
-void dispaly( void )
+void display( void )
 {
-
+	glClear( GL_COLOR_BUFFER_BIT );
+	
+	glutSwapBuffers();
+	glFlush();
 }
 
 void reshape( int w, int h )
 {
-
+	ScreenWidth = w;
+	ScreenHeight = h;
+	
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluOrtho2D( -ViewplaneX, ViewplaneX, -ViewplaneY, ViewplaneY );
+	glViewport( 0, 0, w, h );
 }
 
 void keyboard( unsigned char key, int x, int y )
@@ -34,6 +53,7 @@ void keyboard( unsigned char key, int x, int y )
 		case Minus:
 			break;
 		case J:
+			// Calc/Open Julia set at complex point MouseX + i*MouseY
 			break;
 		case C:
 			break;
@@ -49,25 +69,43 @@ void mouseclick( int button, int state, int x, int y )
 	
 }
 
-void mousedrag( int x, int y )
+void mousemove( int x, int y )
 {
-
+	// track the mouse position to open Julia Set
+	// need to convert between window relative coordinates to
+	// viewplane relative coordinates
+	MouseX = x;
+	MouseY = y;
 }
 
 /*******************************************************************************
  *								Misc. Functions								   *
  ******************************************************************************/
-void initOpenGl( void )
+void initOpenGL( void )
 {
+	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+	
+	// window settings 
+	glutInitWindowSize( ScreenWidth, ScreenHeight );
+	glutInitWindowPosition( 200, 40 );
+	glutCreateWindow( "Mandelbrot and Julia Sets" );
+	
+	// color used to clear screen - black
+	glClearColor( 0.0, 0.0, 0.0, 1.0 );
 
+	// callbacks
+	glutDisplayFunc( display );
+	glutReshapeFunc( reshape );
+	glutKeyboardFunc( keyboard );
+	glutPassiveMotionFunc( mousemove );
 }
 
 float mandelbrot( int x, int y )
 {
-	return 0f;
+	return 0;
 }
 
 float julia( int x, int y )
 {
-	return 0f;
+	return 0;
 }
--- mandelbrot.h
+++ mandelbrot.h
@@ -11,13 +11,22 @@ const int C = 'c';
 const int R = 'r';
 const int A = 'a';
 
+float MouseX = 0;
+float MouseY = 0;
+
+float ScreenWidth = 1200;
+float ScreenHeight = 800;
+float ViewplaneX = 1000;
+float ViewplaneY = 1000;
+
 // OpenGL Callbacks
 void display( void );
 void reshape( int w, int h );
 void keyboard( unsigned char key, int x, int y );
 void mouseclick( int button, int state, int x, int y );
-void mousedrag( int x, int y );
+void mousemove( int x, int y );
 
 // Misc. Functions
+void initOpenGL();
 float mandelbrot( int x, int y );
 float julia( int x, int y );
